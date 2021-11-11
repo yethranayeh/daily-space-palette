9cb0d5a324a918436e6a0c194d38880d00f0e474
yethranayeh <58709123+yethranayeh@users.noreply.github.com>
2021-11-12 00:59:27 +0300

    Added error handling and responsive display so that user can understand what is going on

--- js/generate_palette.js
+++ js/generate_palette.js
@@ -1,86 +1,125 @@
 /** @format */
 
 function check_image_load() {
+	document.getElementById("palettes").innerHTML = `
+	<div class="text-center">
+		<div class="spinner-border" role="status">
+			<span class="sr-only invisible">Loading...</span>
+		</div>
+		<span class="ms-2 lead text-center">Generating palette...</span>
+	</div>
+	`;
 	if (imgSrc) {
 		console.log("Vibrant");
-		Vibrant.from("https://cors-anywhere.herokuapp.com/" + imgSrc).getPalette(function (err, palette) {
-			// Get keys from palette
-			console.log("Err:", err);
-			let keys = Object.keys(palette);
-			console.log(keys);
+		try {
+			Vibrant.from("https://cors-anywhere.herokuapp.com/" + imgSrc).getPalette(function (err, palette) {
+				// Get keys from palette
+				if (err) {
+					console.log("Err:", err);
+					var timeleft = 15;
+					document.getElementById("palettes").innerHTML = `
+							<p id="timer" class="lead text-center">The palette could not be generated. Trying again in ${timeleft} seconds...</p>
+							<p class="lead text-center">It is usually caused by APOD API's Cross-Origin policy. The generation process currently uses a demo proxy to bypass this issue, you may need to request a demo access below to be able to generate the palette on your end:</p>
+							<div class="embed-responsive embed-responsive-16by9">
+								<iframe class="embed-responsive-item w-100 bg-light border rounded" src="https://cors-anywhere.herokuapp.com/corsdemo" allowfullscreen></iframe>
+							</div>
+							`;
+					var downloadTimer = setInterval(function () {
+						if (timeleft <= 0) {
+							clearInterval(downloadTimer);
+							setTimeout(check_image_load(), 1000);
+						} else {
+							// console.log("Current timeleft:", timeleft);
+							document.getElementById(
+								"timer"
+							).innerHTML = `The palette could not be generated. Trying again in ${timeleft} seconds...`;
+							timeleft -= 1;
+						}
+					}, 1000);
+				} else {
+					let keys = Object.keys(palette);
+					console.log(keys);
 
-			counter = 1;
+					counter = 1;
 
-			// Container of all palettes
-			palettesDOM = document.getElementById("palettes");
+					// Container of all palettes
+					palettesDOM = document.getElementById("palettes");
 
-			// Base card section
-			card = `
-                <div class="col-12 col-sm-6 my-4 mx-auto">
-                    <article x-data="{ label: 'HEX', color: '' }" class="card text-dark bg-light">
-                        <div class="card-header">
-                            <div class="form-check form-switch">
-                                <input @click="label = (label == 'RGB') ? 'HEX':'RGB'" class="form-check-input" type="checkbox" id="HEXorRGB_${counter}" autocomplete="off" />
-                                <label x-text="label + color" class="form-check-label" for="HEXorRGB_${counter}">HEX or RGB</label>
-                            </div>
-                        </div>
-                        <div id="palette_${counter}" class="card-body palette-text mx-auto">
-                        </div>
-                    </article>
-                </div>
-                `;
+					// Base card section
+					card = `
+					<div class="col-12 col-sm-6 my-4 mx-auto">
+						<article x-data="{ label: 'HEX', color: '' }" class="card text-dark bg-light">
+							<div class="card-header">
+								<div class="form-check form-switch">
+									<input @click="label = (label == 'RGB') ? 'HEX':'RGB'" class="form-check-input" type="checkbox" id="HEXorRGB_${counter}" autocomplete="off" />
+									<label x-text="label + color" class="form-check-label" for="HEXorRGB_${counter}">HEX or RGB</label>
+								</div>
+							</div>
+							<div id="palette_${counter}" class="card-body palette-text mx-auto">
+							</div>
+						</article>
+					</div>
+					`;
 
-			palettesDOM.innerHTML = palettesDOM.innerHTML + card;
+					palettesDOM.innerHTML = card;
 
-			// For each key (a shade of color)
-			for (const key of keys) {
-				// If the key is not null
-				if (palette[key]) {
-					// RGB Array with values of the color
-					rgb_array = Object.values(palette[key]);
+					// For each key (a shade of color)
+					for (const key of keys) {
+						// If the key is not null
+						if (palette[key]) {
+							// RGB Array with values of the color
+							rgb_array = Object.values(palette[key]);
 
-					// Convert RGB to HEX
-					// Code borrowed from Michal Perlakowski on Stack Overflow
-					const rgbToHex = (r, g, b) => "#" + [r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("");
+							// Convert RGB to HEX
+							// Code borrowed from Michal Perlakowski on Stack Overflow
+							const rgbToHex = (r, g, b) => "#" + [r, g, b].map((x) => x.toString(16).padStart(2, "0")).join("");
 
-					hex = rgbToHex(rgb_array[0][0], rgb_array[0][1], rgb_array[0][2]);
+							hex = rgbToHex(rgb_array[0][0], rgb_array[0][1], rgb_array[0][2]);
 
-					// HTML blocks for each shade of color
-					color_block = `
-                        <div
-                            @mouseover="color = ' | ' + '${key}'"
-                            @mouseout="color = ''"
-                            class="border rounded pt-3"
-                            style="background-color: ${hex}; height: 80px; width: 80px; display: inline-block"
-                        >
-                            <div class="row">
-                                <div class="col text-center text-wrap">
-                                    <div 
-                                        x-data="{copyIcon: ' bi bi-files'}"
-                                        @click="navigator.clipboard.writeText((label == 'HEX') ? '${hex}':'${rgb_array[0]}'); copyIcon = ' bi bi-check2'"
-                                        @mouseout="copyIcon = ' bi bi-files'"
-                                        class="copy-color">
-                                            <i :class="copyIcon"></i>
-                                            Copy
-                                        </div>
-            
-                                    <p x-text="(label == 'HEX') ? '${hex}':'${rgb_array[0]}'" class="mx-auto mt-1 fw-light">#</p>
-                                </div>
-                            </div>
-                        </div>
-                        `;
+							// HTML blocks for each shade of color
+							color_block = `
+							<div
+								@mouseover="color = ' | ' + '${key}'"
+								@mouseout="color = ''"
+								class="border rounded pt-3"
+								style="background-color: ${hex}; height: 80px; width: 80px; display: inline-block"
+							>
+								<div class="row">
+									<div class="col text-center text-wrap">
+										<div 
+											x-data="{copyIcon: ' bi bi-files'}"
+											@click="navigator.clipboard.writeText((label == 'HEX') ? '${hex}':'${rgb_array[0]}'); copyIcon = ' bi bi-check2'"
+											@mouseout="copyIcon = ' bi bi-files'"
+											class="copy-color">
+												<i :class="copyIcon"></i>
+												Copy
+											</div>
+				
+										<p x-text="(label == 'HEX') ? '${hex}':'${rgb_array[0]}'" class="mx-auto mt-1 fw-light">#</p>
+									</div>
+								</div>
+							</div>
+							`;
 
-					// Get palette container
-					const paletteDOM = document.getElementById(`palette_${counter}`);
+							// Get palette container
+							const paletteDOM = document.getElementById(`palette_${counter}`);
 
-					// Insert color blocks to palette
-					paletteDOM.innerHTML = paletteDOM.innerHTML + color_block;
+							// Insert color blocks to palette
+							paletteDOM.innerHTML = paletteDOM.innerHTML + color_block;
+						}
+					}
 				}
-			}
-		});
+			});
+		} catch (error) {
+			alert(
+				"There was an error while generating the palette. Please forward this error to me at github.com/yethranayeh :",
+				error
+			);
+			console.log("Caught error:", error);
+		}
 	} else {
 		console.log("Timeout");
-		window.setTimeout(check_image_load, 1000);
+		window.setTimeout(check_image_load, 5000);
 	}
 }
 
