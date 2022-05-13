6811c4280b0077fc72b52feac5276505337eae64
yethranayeh <aktasalper99@gmail.com>
2022-05-14 01:28:47 +0300

    updated data desc responsive sizing

--- src/styles/DataDescriptionStyled.ts
+++ src/styles/DataDescriptionStyled.ts
@@ -7,4 +7,14 @@ export const DataDescriptionStyled = styled(Text)`
 	color: var(--light);
 	line-height: 1;
 	font-size: 1rem;
+
+	@media (min-width: 992px) {
+		font-size: 1.2rem;
+		line-height: 1.1;
+	}
+
+	@media (min-width: 1200px) {
+		font-size: 1.3rem;
+		line-height: 1.2;
+	}
 `;
