947978f6d56cd9b50c4f3debfe2bf7d459859d73
yethranayeh <aktasalper99@gmail.com>
2022-05-14 01:39:15 +0300

    updated main layout

--- src/styles/ContainerStyled.ts
+++ src/styles/ContainerStyled.ts
@@ -3,9 +3,18 @@
 import styled from "styled-components";
 
 export const ContainerStyled = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 1rem;
 	width: 100%;
 	margin: 0 auto;
 	transition: padding 250ms ease-in-out;
+
+	@media (min-width: 1601px) {
+		padding: 0 12rem;
+	}
 
 	@media (max-width: 1600px) {
 		padding: 0 10rem;
