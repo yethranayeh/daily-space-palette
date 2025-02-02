42e58532b7e36de662a6d2e5e1f872ea0d6f06be
Alper Aktaş <aktasalper99@gmail.com>
2025-02-02 17:15:08 +0300

    add click to copy for swatches

--- app/components/Palette/Palette.tsx
+++ app/components/Palette/Palette.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { generatePalette } from "../lib/generatePalette";
+import { ClickToCopyOverlay } from "./ClickToCopyOverlay";
 
 export async function Palette() {
 	const palette = await generatePalette();
@@ -18,16 +19,34 @@ export async function Palette() {
 				return (
 					<div
 						key={shade}
-						className='flex flex-col items-center justify-center p-4 flex-1'
+						className='relative flex justify-between items-end py-2 px-4 flex-1 cursor-pointer group hover:flex-[3] transition-all'
 						style={{
 							backgroundColor: value.hex
 						}}>
+						<ClickToCopyOverlay value={value.hex} contrastColor={value.titleTextColor} />
+
 						<span className='text-lg font-semibold select-none' style={{ color: value.titleTextColor }}>
 							{shade}
 						</span>
-						<span className='text-sm' style={{ color: value.titleTextColor }}>
-							{value.hex.toUpperCase()}
-						</span>
+
+						<div className='flex justify-between min-w-[150px]'>
+							<div className='flex flex-col justify-around select-none'>
+								<span>HEX</span>
+								<span>RGB</span>
+								<span>HSL</span>
+							</div>
+							<div className='flex flex-col justify-around'>
+								<span className='text-sm' style={{ color: value.titleTextColor }}>
+									{value.hex.toUpperCase()}
+								</span>
+								<span className='text-sm' style={{ color: value.titleTextColor }}>
+									{value.rgb.map((v) => v.toFixed(0)).join(" ")}
+								</span>
+								<span className='text-sm' style={{ color: value.titleTextColor }}>
+									{value.hsl.map((v) => v.toFixed(0)).join(" ")}
+								</span>
+							</div>
+						</div>
 					</div>
 				);
 			})}
