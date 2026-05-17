5f55d73c69f4cf3bcfb2e207bc829230edf29a4f
Alper Halil <aktasalper99@gmail.com>
2026-05-17 13:35:27 +0200

    handle "missing 'sizes' prop" warning

--- app/components/AstronomyPicture/Picture.tsx
+++ app/components/AstronomyPicture/Picture.tsx
@@ -5,5 +5,5 @@ export const Picture = (props: Apod) =>
 	props.media_type === "video" ? (
 		<embed src={props.url} className='w-full h-full object-cover' />
 	) : (
-		<Image fill priority src={props.url} alt={`${props.title} | © · ${props.copyright}`} className='object-cover' />
+		<Image fill priority sizes="(max-width: 768px) 100vw, (max-width: 1440px) 60vw, 880px" src={props.url} alt={`${props.title} | © · ${props.copyright}`} className='object-cover' />
 	);
