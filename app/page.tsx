8a9e2b10d83027f1698f940a502c97f152258937
Alper Halil <aktasalper99@gmail.com>
2026-06-21 16:10:06 +0200

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { generatePalette } from "./lib/generatePalette";
 import { getPicture } from "./lib/getPicture";
-import { getFormattedDate } from "@/app/utils/getFormattedDate";
 import { SpacePaletteLayout } from "./components/SpacePaletteLayout";
 
 export default async function Home() {
   const apod = await getPicture();
   const palette = apod ? await generatePalette(apod) : null;
 
-  return <SpacePaletteLayout apod={apod} palette={palette} date={apod?.date ?? getFormattedDate()} />;
+  return <SpacePaletteLayout apod={apod} palette={palette} date={apod?.date ?? ""} />;
 }
